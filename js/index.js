d1eb58253d493f6402ab1741db8944b93d79cff1
AudreyBalat <46654920+AudreyBalat@users.noreply.github.com>
2020-12-16 13:59:33 +0100

    Declaration d'une fonction + appel

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-//definition d'une variable et affactation du contenu
-var jsload=document.querySelector('#jsload');
-//modif du style
-jsload.style.backgroundColor="RED";
-//modif du contenu
-jsload.innerHTML= 'Mon <span style="font-weight:900"> JS </span> CHARGE';   
+//declaration d'une fonction
+function initialisationJS(prenom){
+    //definition d'une variable et affectation du contenu
+    var jsload=document.querySelector('#jsload');
+    //modif du style
+    jsload.style.backgroundColor="RED";
+    //modif du contenu
+    jsload.innerHTML= 'Mon <span style="font-weight:900"> JS </span> CHARGE pour '+prenom; 
+}
+//usage d'une fonction
+initialisationJS('Audrey');
+  
