7c30400f7b6cb99f666e2c857bb181a5d4e5e970
AudreyBalat <46654920+AudreyBalat@users.noreply.github.com>
2020-12-16 14:43:19 +0100

    Execution apres la fin du chargement du DOM

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+//declaration d'une fonction anonyme
+addEventListener('load', function(event) { //window.addEvent ... Comme window pas besoin  de preciser
+    //usage d'une fonction
+    initialisationJS('Audrey');
+    //accrochage d'un ecouteur d'event sur une balise
+    //event : submit
+    //fonction à déclencher pour l'event -W formSubmited
+    document.querySelector('form').addEventListener('submit', formSubmited); // Pas formSubmited() car sinon la fonction va s'exécuter, or elle doit s'executer uniquement lors de l'appel
+})
+
 //declaration d'une fonction
 function initialisationJS(prenom){
     //definition d'une variable et affectation du contenu
@@ -7,6 +17,15 @@ function initialisationJS(prenom){
     //modif du contenu
     jsload.innerHTML= 'Mon <span style="font-weight:900"> JS </span> CHARGE pour '+prenom; 
 }
-//usage d'une fonction
-initialisationJS('Audrey');
-  
+
+//fonction formSubmited
+function formSubmited(evt) {
+    evt.preventDefault();
+    console.log('Mon formulaire est "submit"');
+    //console.log(evt);
+    console.log(evt.target[0].value);
+    console.log(evt.target[1].value);
+    console.log(evt.target[2].value);
+    console.log(evt.target[3].value);
+    console.log(evt.target[0].value);
+}
