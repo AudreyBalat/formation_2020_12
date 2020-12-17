4e48b8bfc29bb89dc53bed8c08af871b4a4b6c47
AudreyBalat <46654920+AudreyBalat@users.noreply.github.com>
2020-12-17 14:52:31 +0100

    Fabrication d'un plan objet cred

--- js/rest.js
+++ js/rest.js
@@ -1,89 +1,95 @@
-//declaration chemin d'accès
+//declaration constante d'url de l'appli
 var BASE_URL='http://localhost:3133'
+//declaration d'un créateur d'objet
 /**
- * Permet l'appel HTTP avec XMLHTttpRequest
- * @param {*} ressourceUrl url de la ressource
+ * Objet permettant les appels htpp
+ * @param {*} baseurl base de l'url des ressources
  */
-function get(ressourceUrl) {
-    //instanciation de XHR
-    var xhr=new XMLHttpRequest();
-    //ouverture de la connexion
-    xhr.open('GET', BASE_URL+ressourceUrl);
-    //tache à effectuer à chaquechangement de readstate (passa d'une étape de reception)
-    // 1 -> open 2 -> send 3 -> en crours de recetption 4 -> fin de reception
-    xhr.onreadystatechange=function(evt){
-        if(evt.currentTarget.readyState<XMLHttpRequest.DONE){
-            return;
+var Crud=function name(baseurl) {
+    /**
+     * Permet l'appel HTTP avec XMLHTttpRequest
+     * @param {*} ressourceUrl url de la ressource
+     */
+    function get(ressourceUrl) {
+        //instanciation de XHR
+        var xhr=new XMLHttpRequest();
+        //ouverture de la connexion
+        xhr.open('GET', Bbaseurl+ressourceUrl);
+        //tache à effectuer à chaquechangement de readstate (passa d'une étape de reception)
+        // 1 -> open 2 -> send 3 -> en crours de recetption 4 -> fin de reception
+        xhr.onreadystatechange=function(evt){
+            if(evt.currentTarget.readyState<XMLHttpRequest.DONE){
+                return;
+            }
+            var objt=JSON.parse(evt.currentTarget.response);
+            console.log(objt);
         }
-        var objt=JSON.parse(evt.currentTarget.response);
-        console.log(objt);
+        //envoie de la requête
+        xhr.send();
     }
-    //envoie de la requête
-    xhr.send();
-}
-/**
- * Permet l'envoi en POST d'une ressource sur l'ressource Url
- * @param {*} ressourceUrl chemin du post
- * @param {*} ressource data a envoyé
- */
-function post(ressourceUrl, ressource){
-    //instanciation de XHR
-    var xhr=new XMLHttpRequest();
-    //ouverture de la connexion
-    xhr.open('POST', BASE_URL+ressourceUrl);
-    //instanciation des headers
-    //specification du type contenu
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    //specification de ce qui est attendu en retour
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onreadystatechange=function(evt){
-        if(xhr.readyState<4){
-            return;
+    /**
+     * Permet l'envoi en POST d'une ressource sur l'ressource Url
+     * @param {*} ressourceUrl chemin du post
+     * @param {*} ressource data a envoyé
+     */
+    function post(ressourceUrl, ressource){
+        //instanciation de XHR
+        var xhr=new XMLHttpRequest();
+        //ouverture de la connexion
+        xhr.open('POST', baseurl+ressourceUrl);
+        //instanciation des headers
+        //specification du type contenu
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        //specification de ce qui est attendu en retour
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.onreadystatechange=function(evt){
+            if(xhr.readyState<4){
+                return;
+            }
+            console.log(JSON.parse(xhr.response));
         }
-        console.log(JSON.parse(xhr.response));
+        //transformation en JSON du contenu Objet
+        xhr.send(JSON.stringify(ressource));
     }
-    //transformation en JSON du contenu Objet
-    xhr.send(JSON.stringify(ressource));
-}
-/**
- * Permet l'envoie du DELETE
- * @param {*} ressourceUrl 
- */
-function remove(ressourceUrl) {
-    //instanciation de XHR
-    var xhr=new XMLHttpRequest();
-    //ouverture de la connexion
-    xhr.open('DELETE', BASE_URL+ressourceUrl);
-    xhr.onreadystatechange=function(evt){
-        if(xhr.readyState<4){
-            return;
+    /**
+     * Permet l'envoie du DELETE
+     * @param {*} ressourceUrl 
+     */
+    function remove(ressourceUrl) {
+        //instanciation de XHR
+        var xhr=new XMLHttpRequest();
+        //ouverture de la connexion
+        xhr.open('DELETE', baseurl+ressourceUrl);
+        xhr.onreadystatechange=function(evt){
+            if(xhr.readyState<4){
+                return;
+            }
+            console.log(JSON.parse(xhr.response));
         }
-        console.log(JSON.parse(xhr.response));
+        xhr.send();
     }
-    xhr.send();
-}
-/**
- * Permet l'envoi en PUT d'une ressource sur l'ressource Url
- * @param {*} ressourceUrl chemin du post
- * @param {*} ressource data a envoyé
- */
-function put(ressourceUrl, ressource){
-    //instanciation de XHR
-    var xhr=new XMLHttpRequest();
-    //ouverture de la connexion
-    xhr.open('PUT', BASE_URL+ressourceUrl);
-    //instanciation des headers
-    //specification du type contenu
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    //specification de ce qui est attendu en retour
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onreadystatechange=function(evt){
-        if(xhr.readyState<4){
-            return;
+    /**
+     * Permet l'envoi en PUT d'une ressource sur l'ressource Url
+     * @param {*} ressourceUrl chemin du post
+     * @param {*} ressource data a envoyé
+     */
+    function put(ressourceUrl, ressource){
+        //instanciation de XHR
+        var xhr=new XMLHttpRequest();
+        //ouverture de la connexion
+        xhr.open('PUT', baseurl+ressourceUrl);
+        //instanciation des headers
+        //specification du type contenu
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        //specification de ce qui est attendu en retour
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.onreadystatechange=function(evt){
+            if(xhr.readyState<4){
+                return;
+            }
+            console.log(JSON.parse(xhr.response));
         }
-        console.log(JSON.parse(xhr.response));
-    }
-    //transformation en JSON du contenu Objet
-    xhr.send(JSON.stringify(ressource));
+        //transformation en JSON du contenu Objet
+        xhr.send(JSON.stringify(ressource));
+    }  
 }
-get('/postit/1');
