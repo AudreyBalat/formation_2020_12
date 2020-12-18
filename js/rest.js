b69295a54c4e02aece6ebc0b638f2fe46ae80d14
AudreyBalat <46654920+AudreyBalat@users.noreply.github.com>
2020-12-18 09:39:55 +0100

    Ajout callback

--- js/rest.js
+++ js/rest.js
@@ -10,7 +10,7 @@ var Crud=function name(baseurl) {
      * Permet l'appel HTTP avec XMLHTttpRequest
      * @param {*} ressourceUrl url de la ressource
      */
-    function get(ressourceUrl, callback) {
+    function _get(ressourceUrl, callback) {
         //instanciation de XHR
         var xhr=new XMLHttpRequest();
         //ouverture de la connexion
@@ -33,7 +33,7 @@ var Crud=function name(baseurl) {
      * @param {Uri} ressourceUrl chemin du post
      * @param {Object} ressource data a envoyé
      */
-    function post(ressourceUrl, ressource){
+    function _post(ressourceUrl, ressource){
         //instanciation de XHR
         var xhr=new XMLHttpRequest();
         //ouverture de la connexion
@@ -57,7 +57,7 @@ var Crud=function name(baseurl) {
      * @param {Uri} ressourceUrl adresse de la ressource
      * @param {Function} callback fonction à executer à la fin de la suppression
      */
-    function remove(ressourceUrl, callback) {
+    function _remove(ressourceUrl, callback) {
         //instanciation de XHR
         var xhr=new XMLHttpRequest();
         //ouverture de la connexion
@@ -76,7 +76,7 @@ var Crud=function name(baseurl) {
      * @param {Uri} ressourceUrl chemin du post
      * @param {Object} ressource data a envoyé
      */
-    function put(ressourceUrl, ressource){
+    function _put(ressourceUrl, ressource){
         //instanciation de XHR
         var xhr=new XMLHttpRequest();
         //ouverture de la connexion
@@ -97,8 +97,8 @@ var Crud=function name(baseurl) {
     }  
     //zone d'exposition des fonctions en public
     //pour acces depuis l'exterieur de l'instance
-    this.recuperer=get;
-    this.creer=post;
-    this.maj=put;
-    this.supprimer=remove;
+    this.recuperer=_get;
+    this.creer=_post;
+    this.maj=_put;
+    this.supprimer=_remove;
 }
